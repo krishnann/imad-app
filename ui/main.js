4911f6847cd19cf818d5c7071dc7552d9dd9eefe
krishnann <30865333+krishnann@users.noreply.github.com>
2017-09-02 12:11:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 
 //change the content of html
-var element = document.getElementById("element");
+var element = document.getElementById('element');
 
 element.innerHTML = 'hello world!';
