30cc31fc4a890dfaebe4b9df35999baae1759f77
krishnann <30865333+krishnann@users.noreply.github.com>
2017-09-03 18:09:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,25 @@ var button = document.getElementById('counter');
 var count = 0;
 
 button.onclick = function(){
-    count = count + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = count.toString();
+    // count = count + 1;
+    // var span = document.getElementById('count');
+    // span.innerHTML = count.toString();
+    
+    //make the request to the counter end point
+    var request = new XMLhttpRequest();
+    
+    //if the state of the request change 
+    request.onreadystatechange = function(){
+        //if the request has been successfully done
+        if(request.readystate === XMLhttpRequest.DONE){
+            //request has been successfully completed
+            if(request.status === 200){
+                //take the response from the request
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+                
+            }
+        }
+    }
 };
