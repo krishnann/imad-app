4c8a511f4cdb330b21a243e94c6bc8f3fc01f48a
krishnann <30865333+krishnann@users.noreply.github.com>
2017-09-03 14:25:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 //incrementing the counter
 var count = 0;
-var button = document.getElementById('cntrbtn');
+var button = document.getElementById('counter');
 button.onclick = function(){
     count = count + 1;
-    var data = document.getElementById('cntr');
+    var data = document.getElementById('count');
     data.innerHTML = data.toString();
 };
