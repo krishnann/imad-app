1060bc17fc4610ea69701ca0cf9f478410a1f036
krishnann <30865333+krishnann@users.noreply.github.com>
2017-09-06 18:59:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,30 @@
 //incrementing the counter
-// var button = document.getElementById('counter');
-// var count = 0;
+var button = document.getElementById('counter');
+var count = 0;
 
-// button.onclick = function(){
+button.onclick = function(){
    
-//     //make the request to the counter end point
-//     var request = new XMLHttpRequest();
+    //make the request to the counter end point
+    var request = new XMLHttpRequest();
     
-//     //if the state of the request change 
-//     request.onreadystatechange = function(){
-//         //if the request has been successfully done
-//         if(request.readyState === XMLHttpRequest.DONE){
-//             //request has been successfully completed
-//             if(request.status === 200){
-//                 //take the response from the request
-//                 var counter = request.responseText;
-//                 var span = document.getElementById('count');
-//                 span.innerHTML = counter.toString();
-//             }
-//         }
-//     };
+    //if the state of the request change 
+    request.onreadystatechange = function(){
+        //if the request has been successfully done
+        if(request.readyState === XMLHttpRequest.DONE){
+            //request has been successfully completed
+            if(request.status === 200){
+                //take the response from the request
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
     
-//     //make request
-//     request.open('GET', 'http://knaik0901.imad.hasura-app.io/counter', true);
-//     request.send(null);
-// };
+    //make request
+    request.open('GET', 'http://knaik0901.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
 
 
 
