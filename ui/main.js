d0f2182cb413933c4894b7d15bea9c1d4f42f7a4
krishnann <30865333+krishnann@users.noreply.github.com>
2017-09-06 19:18:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -61,7 +61,7 @@ btn.onclick = function(){
     };
     
     //make request
-    request.open('GET', 'http://knaik0901.imad.hasura-app.io/send_name?name='+naam, true);
+    request.open('GET', 'http://knaik0901.imad.hasura-app.io/send_name?name='+username, true);
     request.send(null);
 
 };
